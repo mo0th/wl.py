d9d711bae413e6ac83691e28c5316cdd99effd3e
Soorria Saruva <soorria.ss@gmail.com>
2020-09-19 18:13:40 +1000

    initial commit

---
+++ cli.py
@@ -0,0 +1,127 @@
+import argparse
+import os
+from os import path
+
+from wl import Watchlist, InvalidStatusError, ItemNotFoundError, STATUSES
+
+
+def print_item(name, status):
+    print(f'{name} => {status}')
+
+
+def wl_status(status: str) -> str:
+    status = status.lower()
+    if status not in STATUSES:
+        raise argparse.ArgumentTypeError(
+            "must be one of " + ', '.join(STATUSES))
+
+    return status
+
+
+wl = None
+fieldnames = ['name', 'status']
+
+filename = os.path.expanduser('~/wl')
+if 'WL_PATH' in os.environ:
+    filename = os.path.expanduser(os.environ['WL_PATH'])
+
+read_required = False
+write_required = True
+
+parser = argparse.ArgumentParser()
+
+subparsers = parser.add_subparsers(
+    dest='command', help='thingo', required=True)
+
+add_parser = subparsers.add_parser('add', help='add an item')
+add_parser.add_argument('name', help="name of the new item")
+add_parser.add_argument(
+    '-s', '--status',
+    help='status to filter by',
+    default='unwatched',
+    type=wl_status
+)
+
+remove_parser = subparsers.add_parser('remove', help='remove an item')
+remove_parser.add_argument('name', help='name of the item to remove')
+
+update_parser = subparsers.add_parser(
+    'update',
+    help='update the status of an item'
+)
+update_parser.add_argument('name', help="name of the item to update")
+update_parser.add_argument(
+    'status',
+    help="updated status of the item",
+    default="watched",
+    nargs="?",
+    type=wl_status
+)
+
+search_parser = subparsers.add_parser('search', help="search for an item")
+search_parser.add_argument('search', help='search string')
+search_parser.add_argument(
+    '-s', '--status',
+    help='status to filter by',
+    default='unwatched',
+    type=wl_status
+)
+
+list_parser = subparsers.add_parser('list', help='list all items')
+
+summary_parser = subparsers.add_parser(
+    'summary',
+    help="show a summary of your watch list"
+)
+
+args = parser.parse_args()
+
+print(args)
+
+try:
+    wl = Watchlist.from_file(filename)
+except FileNotFoundError:
+    wl = Watchlist()
+
+if args.command == 'add':
+    print(3)
+    wl.add(args.name, args.status)
+    write_required = True
+
+elif args.command == 'update':
+    wl.update(args.name, args.status)
+    write_required = True
+
+elif args.command == 'remove':
+    wl.remove(args.name)
+    write_required = True
+
+elif args.command == 'list':
+    for item in wl:
+        print_item(**item)
+
+elif args.command == 'search':
+    results = wl.search(args.search, args.status)
+    for item in results:
+        print_item(**item)
+
+elif args.command == 'summary':
+    total = 0
+    summary = wl.summary()
+    status_col_width = len(max(STATUSES, key=len)) + 1
+    count_col_width = len(str(max(summary.values())))
+
+    for status, count in summary.items():
+        total += count
+        print(
+            f' {status.ljust(status_col_width)} | {str(count).rjust(count_col_width)}'
+        )
+
+    print('-'*(status_col_width + count_col_width + 3 + 2))
+
+    print(f' {"total".ljust(status_col_width)} | {str(total).rjust(count_col_width)}')
+
+
+if wl and write_required:
+    print('saving...')
+    wl.to_file(filename)
---
+++ wl.py
@@ -0,0 +1,107 @@
+import csv
+from typing import List, Dict, Iterator, Iterable
+import typing
+
+FIELD_NAMES = ['name', 'status']
+STATUSES = ['unwatched', 'watching', 'watched', 'on hold', 'dropped']
+
+WatchListItem = Dict[str, str]
+
+
+class ItemNotFoundError(Exception):
+    """Exception raised when no matching item is found"""
+
+
+class InvalidStatusError(Exception):
+    """Exception raised when an invalid status is used"""
+
+
+class Watchlist:
+    def __init__(self, items=None):
+        if items == None:
+            self._items = []
+            self._len = 0
+        else:
+            self._items = items
+            self._len = len(self._items)
+
+    def add(self, name: str, status: str = None):
+        print(1)
+        if status is None:
+            status = 'unwatched'
+        elif status not in STATUSES:
+            raise InvalidStatusError
+
+        for item in self.search(name):
+            if item['name'] == name:
+                confirm = input(f"'{name}' already exists. Add anyway? [y/N]")
+                if confirm.lower() not in ['y', 'ye', 'yes']:
+                    return print("No items were added")
+
+        self._items.append({'name': name, 'status': status})
+        self._len += 1
+
+    @property
+    def items(self):
+        return self._items
+
+    def search(self, search_string, status=None) -> Iterable[WatchListItem]:
+        if status is None:
+            return filter(lambda i: search_string in i['name'], self)
+        elif status not in STATUSES:
+            raise InvalidStatusError
+        else:
+            return filter(lambda i: search_string in i['name'] and i['status'] == status, self)
+
+    def update(self, name, status):
+        if status is None:
+            status = 'watched'
+        elif status not in STATUSES:
+            raise InvalidStatusError
+
+        for item in self._items:
+            if item['name'] == name:
+                item['status'] = status
+                break
+        else:
+            raise ItemNotFoundError
+
+    def remove(self, name: str):
+        self._items = [i for i in self._items if i['name'] != name]
+
+        if len(self._items) == self._len:
+            raise ItemNotFoundError
+
+        self._len -= 1
+
+    def summary(self, statuses=None) -> Dict[str, int]:
+        if statuses is None:
+            statuses = STATUSES
+
+        result = dict.fromkeys(statuses, 0)
+
+        for item in self._items:
+            if item['status'] in statuses:
+                result[item['status']] += 1
+
+        return result
+
+    def to_file(self, filename: str):
+        print(2)
+        with open(filename, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=FIELD_NAMES)
+
+            writer.writerows(self._items)
+
+    @staticmethod
+    def from_file(filename) -> List[WatchListItem]:
+        items = None
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f, fieldnames=FIELD_NAMES)
+            return Watchlist([r for r in reader])
+
+    def __iter__(self) -> Iterator[WatchListItem]:
+        return iter(self._items)
+
+    def __len__(self) -> int:
+        return self._len
